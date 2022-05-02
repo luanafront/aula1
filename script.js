f3ed86d82946bfc0f3bf74f1567039ffbfb34689
luanafront <luana.lopes.rodrigues1@gmail.com>
2022-05-02 16:40:25 -0300

    coloquei as class principais do html: cabecalgo, principal e footer

---
+++ script.js
@@ -0,0 +1,8 @@
+const root = document.createElement("root")
+root.appendChild(root)
+console.log(root)
+
+// const cabecalho = document.createElement("header")
+// cabecalho.classList.add("cabecalho")
+// root.appendChild(cabecalho)
+
